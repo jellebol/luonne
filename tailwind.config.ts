ffe133871cadf060dca76be85afa1a2b8b6513f2
jellebol <muriellesmit@gmail.com>
2023-08-16 15:39:15 +0200

    Tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    colors: {
+      blue: '#6357b1',
+      purple: '#6357b1',
+      green: '#25ff90',
+      'gray-dark': '#4b4b4b',
+      gray: '#787878',
+      'gray-light': '#f6f6f6',
+    },
+  },
+  plugins: [],
+} satisfies Config
+
