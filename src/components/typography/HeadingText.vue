4d344e6ed3a30ec0f3b300b6a27829a265f0829e
jellebol <muriellesmit@gmail.com>
2023-08-17 13:16:58 +0200

    Add font size to heading component

--- src/components/typography/HeadingText.vue
+++ src/components/typography/HeadingText.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 interface Props {
   title: string
-  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+  type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
   isInvertColour?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   type: 'h2'
 })
+
+const fontSizeClass = computed<string>(() => {
+  switch (props.type) {
+    case 'h1':
+      return 'text-2xl'
+    case 'h2':
+      return 'text-xl'
+    default:
+      return 'text-lg'
+  }
+})
 </script>
 
 <template>
-  <component :is="props.type" :class="{ 'text-white': props.isInvertColour }">
+  <component :is="props.type" :class="[{ 'text-white': props.isInvertColour }, fontSizeClass]">
     {{ props.title }}
   </component>
 </template>
