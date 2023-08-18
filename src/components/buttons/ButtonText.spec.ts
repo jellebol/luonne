e8afc1cbdc687d251798ba806a0d21e4b96eef88
jellebol <muriellesmit@gmail.com>
2023-08-18 17:30:52 +0200

    Add ButtonText component

---
+++ src/components/buttons/ButtonText.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { shallowMount } from '@vue/test-utils'
+import ButtonText from './ButtonText.vue'
+
+describe('ButtonText', () => {
+  it('renders text', () => {
+    const wrapper = shallowMount(ButtonText, { props: { text: 'Click' } })
+    expect(wrapper.text()).toContain('Click')
+  })
+})
---
+++ src/components/buttons/ButtonText.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <button
+    @click="$emit('clickEvent')"
+    type="button"
+    class="py-2 px-3 rounded bg-green hover:bg-white border-2 border-green focus:outline outline-purple-light outline-2 font-medium transition-all"
+  >
+    {{ props.text }}
+  </button>
+</template>
